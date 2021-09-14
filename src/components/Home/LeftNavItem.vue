9dc30c2e5be3689dc3403bb837aee7532e0c09d2
”yk2001525“ <1272689242@qq.com>
2021-09-14 21:44:54 +0800

    09月14日21:44:50

--- src/components/Home/LeftNavItem.vue
+++ src/components/Home/LeftNavItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="root">
-    <img  v-show="isshow" src="../../assets/nav-item.png" alt="" />
+    <img v-show="isshow" src="../../assets/nav-item.png" alt="" />
     <div
       @mouseleave="mouseleave"
       @mouseenter="mouseenter"
@@ -14,47 +14,209 @@
         <slot name="item"></slot>
       </div>
     </div>
-    <div style="z-index:1" @mouseenter="mouseenter" @mouseleave="mouseleave" v-show="isshow" class="banner-detail">
-        <div class="detail-item">1</div>
-        <div class="detail-item">2</div>
-        <div class="detail-item">3</div>
-        <div class="brand">4</div>
+    <div
+      style="z-index:1"
+      @mouseenter="mouseenter"
+      @mouseleave="mouseleave"
+      v-show="isshow"
+      class="banner-detail"
+    >
+      <div class="detail-item">
+        <span>
+            <div class="detail-item-title-top">进口牛奶</div>
+            <div class="item-row">
+              <div class="detail-item-message">进口纯牛奶</div>
+              <div class="detail-item-message">进口酸奶</div>
+            </div>
+            <div class="item-row">
+              <div class="detail-item-message">进口成人奶粉</div>
+              <div class="detail-item-message">进口含乳饮料</div>
+            </div>
+            <div class="item-row">
+              <div class="detail-item-message">进口有机牛奶</div>
+              <div class="detail-item-message">进口酸奶粉</div>
+            </div>
+   
+            <div class="detail-item-title">进口粮油/速食/调料</div>
+            <div class="item-row">
+              <div class="detail-item-message">进口橄榄油</div>
+              <div class="detail-item-message">进口其他油</div>
+              <div class="detail-item-message">进口大米</div>
 
+            </div>
+            <div class="item-row">
+              <div class="detail-item-message">进口方便面</div>
+              <div class="detail-item-message">进口意面</div>
+              <div class="detail-item-message">其他速食</div>
+
+            </div>
+            <div class="item-row">
+              <div class="detail-item-message">进口罐头</div>
+              <div class="detail-item-message">进口调味料</div>
+              <div class="detail-item-message">进口调味酱</div>
+
+            </div>
+    
+            <div class="detail-item-title">进口厨房用品</div>
+            <div class="item-row">
+              <div class="detail-item-message">进口餐具/水杯</div>
+              <div class="detail-item-message">进口厨具/锅具</div>
+            </div>
+            <div class="item-row">
+                <div class="detail-item-message">进口一次性用品</div>
+
+            </div>
+        
+        </span>
+      </div>
+      <div class="detail-item">
+            <span>
+            <div class="detail-item-title-top">进口零食</div>
+            <div class="item-row">
+              <div class="detail-item-message">进口蜜饯果干</div>
+              <div class="detail-item-message">进口薯片/膨化</div>
+            </div>
+            <div class="item-row">
+              <div class="detail-item-message">进口坚果</div>
+              <div class="detail-item-message">进口糖果</div>
+              <div class="detail-item-message">进口果冻布丁</div>
+            </div>
+            <div class="item-row">
+              <div class="detail-item-message">进口海产品</div>
+              <div class="detail-item-message">进口肉干</div>
+              <div class="detail-item-message">进口巧克力</div>
+            </div>
+            <div class="detail-item-title">进口母婴用品</div>
+            <div class="item-row">
+              <div class="detail-item-message">进口纸尿裤</div>
+              <div class="detail-item-message">进口母婴洗护</div>
+            </div>
+            <div class="item-row">
+              <div class="detail-item-message">进口婴儿奶粉</div>
+              <div class="detail-item-message">进口辅食</div>
+            </div>
+            <div class="item-row">
+              <div class="detail-item-message">进口幼婴儿营养品</div>
+            </div>
+            <div class="detail-item-title">进口饼干/糕点</div>
+            <div class="item-row">
+              <div class="detail-item-message">进口饼干</div>
+              <div class="detail-item-message">进口糕点</div>
+              <div class="detail-item-message">进口面包干</div>
+
+            </div>
+            <div class="item-row">
+              <div class="detail-item-message">进口威化</div>
+              <div class="detail-item-message">进口苏打饼干</div>
+              <div class="detail-item-message">进口曲奇</div>
+            </div>
+        </span>
+      </div>
+      <div class="detail-item">
+            <span>
+            <div class="detail-item-title-top">进口酒饮</div>
+            <div class="item-row">
+              <div class="detail-item-message">进口葡萄酒</div>
+              <div class="detail-item-message">进口啤酒</div>
+              <div class="detail-item-message">进口起泡酒</div>
+
+            </div>
+            <div class="item-row">
+              <div class="detail-item-message">进口洋酒</div>
+              <div class="detail-item-message">进口果汁</div>
+              <div class="detail-item-message">进口水</div>
+
+            </div>
+            <div class="item-row">
+              <div class="detail-item-message">进口咖啡饮料</div>
+              <div class="detail-item-message">进口碳酸饮料</div>
+            </div>
+   
+            <div class="detail-item-title">进口冲调保健</div>
+            <div class="item-row">
+              <div class="detail-item-message">进口咖啡</div>
+              <div class="detail-item-message">进口茶/蜂蜜</div>
+            </div>
+            <div class="item-row">
+              <div class="detail-item-message">进口谷物冲饮</div>
+              <div class="detail-item-message">进口奶茶</div>
+              <div class="detail-item-message">进口茶叶</div>
+
+            </div>
+            <div class="item-row">
+              <div class="detail-item-message">进口保健品</div>
+              <div class="detail-item-message">进口维生素</div>
+            </div>
+    
+            <div class="detail-item-title">进口个人护理</div>
+            <div class="item-row">
+              <div class="detail-item-message">进口洗发水</div>
+              <div class="detail-item-message">进口护发</div>
+              <div class="detail-item-message">进口沐浴</div>
+
+            </div>
+            <div class="item-row">
+              <div class="detail-item-message">进口私处洗液</div>
+              <div class="detail-item-message">进口牙膏</div>
+              <div class="detail-item-message">进口牙刷</div>
+              
+            </div>
+            <div class="item-row">
+              <div class="detail-item-message">进口漱口水</div>
+              <div class="detail-item-message">进口面部护理</div>
+            </div>
+        </span>
+      </div>
+      <div class="brand detail-item">
+            <span>
+              
+            <div class="detail-item-title-top">品牌</div>
+            <div class="item-row">
+              <div class="detail-item-message">安佳牛奶</div>
+              <div class="detail-item-message">中原G7</div>
+            </div>
+            <div class="item-row">
+              <div class="detail-item-message">家乐氏</div>
+              <div class="detail-item-message">巴黎水</div>
+            </div>
+            <div class="item-row">
+              <div class="detail-item-message">汤姆农场</div>
+              <div class="detail-item-message">小老板</div>
+            </div>
+   
+            <div style="color:#E22a40" class="detail-item-title">品牌活动</div>
+            <div class="item-row">
+              <div class="detail-item-message">德亚牛奶</div>
+              <div class="detail-item-message">进口谷物冲饮</div>
+            </div>
+        </span>
+      </div>
     </div>
-
-
   </div>
 </template>
 
 <script>
-
 export default {
-    components:{
-
-    },
+  components: {},
   data() {
     return {
       isshow: false,
-
     };
   },
   methods: {
     mouseenter() {
-             this.isshow = true;
-
+      this.isshow = true;
     },
     mouseleave() {
       this.isshow = false;
-
     },
-
   },
 };
 </script>
 
 <style lang="scss" scoped>
 .root {
-    /* display: block; */
+  /* display: block; */
   /* margin: 0 auto; */
   /* width: 1190px; */
   margin-top: 1px;
@@ -78,10 +240,9 @@ export default {
       transition: all 0.1s;
       color: #515151;
       font-size: 14px;
-        span{
-            color: #515151!important;
-        }
-
+      span {
+        color: #515151 !important;
+      }
     }
     .item {
       transition: all 0.1s;
@@ -91,8 +252,6 @@ export default {
       font-size: 12px;
       margin-top: 10px;
       margin-left: 30px;
-
-
     }
   }
   .detail-active {
@@ -127,23 +286,57 @@ export default {
     box-sizing: border-box;
     border: 3px solid #e22a40;
     /* display: flex; */
-    .detail-item{
-        padding: 15px 20px;
-        box-sizing: border-box;
-        display: inline-block;
-        width: 248px;
-        height: 100%;
-        border-right: 1px dotted #d3d3d3;
-        background-color: #fff;
+    overflow: hidden;
+    .detail-item {
+      padding: 15px 20px;
+      box-sizing: border-box;
+      display: inline-block;
+      width: 248px;
+      height: 100%;
+      border-right: 1px dotted #d3d3d3;
+      background-color: #fff;
+      overflow: hidden;
+      .detail-item-title {
+        font-size: 12px;
+        font-weight: 700;
+        margin-top: 20px;
+      }
+      .detail-item-title-top {
+        font-size: 12px;
+        font-weight: 700;
+        margin-top: 0px;
+      }
+      .item-row {
+        width: 225px;
+        .detail-item-message:first-child {
+          border-left: 0;
+        }
+        .detail-item-message:hover{
+          cursor: pointer;
+          color: #e22a40;
+          text-decoration: underline;
+        }
+        .detail-item-message {
+          
+          font-size: 12px;
+          margin-top: 15px;
+          color: #666;
+          border-left: 1px solid #e7e7e7;
+          display: inline-block;
+          padding-left: 9px;
+          margin-left: -10px;
+          margin-right: 18px;
+        }
+      }
     }
-    .brand{
-        display: inline-block;
-        box-sizing: border-box;
-        padding: 15px 20px;
-        height: 100%;
-        width: 197px;
-        background-color: #f7f7f7;
-
+    .brand {
+      display: inline-block;
+      box-sizing: border-box;
+      padding: 15px 20px;
+      height: 100%;
+      width: 197px;
+      background-color: #f7f7f7;
+      overflow: hidden;
     }
   }
 }
--- src/components/Position.vue
+++ src/components/Position.vue
@@ -2,17 +2,25 @@
       <div class="position">
       <div @mouseenter="cityshow=true" @mouseleave="cityshow=false" :style="{color:color}" class="detail">
         <div v-show="cityshow" class="cityselect">
+          <div style="position:absolute;width:72px;background-color:#fff;top:-2px;height:10px"></div>
           <div class="top">
             热门城市:
             <div class="top-detail">
-              <span>北京</span><span>北京</span><span>北京</span
-              ><span>北京</span><span>北京</span><span>北京</span>
+              <span>北京</span><span>上海</span><span>杭州</span
+              ><span>广州</span><span>天津</span><span>苏州</span>
               <span>成都</span>
-              <span>成都</span>
-              <span>成都</span>
-              <span>成都</span>
-              <span>成都</span>
-              <span>成都</span>
+              <span>深圳</span>
+              <span>南京</span>
+              <span>重庆</span>
+              <span>南昌</span>
+              <span>武汉</span>
+            </div>
+            <div class="select-area">
+                <div @mouseenter="activeIndex = 0" :class="activeIndex == 0 ?  'select-active':''">ABCDE</div>
+                <div  @mouseenter="activeIndex = 1" :class="activeIndex == 1 ?  'select-active':''" >FGHJ</div>
+                <div  @mouseenter="activeIndex = 2" :class="activeIndex == 2 ?  'select-active':''" >KLMNP</div>
+                <div  @mouseenter="activeIndex = 3" :class="activeIndex == 3 ?  'select-active':''" >QRSTW</div>
+                <div  @mouseenter="activeIndex = 4" :class="activeIndex == 4 ?  'select-active':''" >XYZ</div>
             </div>
           </div>
           <div class="center"></div>
@@ -32,7 +40,8 @@ export default {
     },
     data(){
         return {
-            cityshow:false
+            cityshow:false,
+            activeIndex:0
         }
     }
 
@@ -43,7 +52,9 @@ export default {
  .position {
     display: inline-block;
     margin-top: 10px;
-
+    .detail:hover{
+      /* border-bottom: 0; */
+    }
     .detail {
       border: 1px solid #e5e5e5;
       font-size: 16px;
@@ -52,6 +63,7 @@ export default {
       padding: 8px 12px;
       border-radius: 3px;
       position: relative;
+      box-sizing: border-box;
       .cityselect {
         width: 482px;
         height: 795px;
@@ -79,6 +91,29 @@ export default {
               color: #333;
               width: 48px;
               display: inline-block;
+            }
+          }
+          .select-area{
+            margin-top: 25px;
+            display: flex;
+            font-size: 14px;
+            color: black;
+            justify-content: space-around;
+            border-bottom: 1px solid rgb(238, 238, 238);
+            position: relative;
+            left: -12px;
+              box-sizing: border-box;
+
+            div{
+              height: 43px;
+              line-height: 43px;
+              box-sizing: border-box;
+              padding: 0 10px 0;
+            }
+            .select-active{
+              box-sizing: border-box;
+              border: 1px solid #e5e5e5;
+              border-bottom: 1px solid #fff;
             }
           }
         }
